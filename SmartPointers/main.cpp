7e8aba58a1b1ec1a69395447017f251c5808fb84
Artur Hebda <artur@adtaily.com>
2011-06-14 16:33:47 +0200

    Initial commit

--- SmartPointers/main.cpp
+++ SmartPointers/main.cpp
@@ -2,17 +2,33 @@
 //  main.cpp
 //  SmartPointers
 //
-//  Created by Artur Hebda on 14/06/2011.
+//  Created by Artur Hebda on 13/06/2011.
 //  Copyright 2011 AdTaily. All rights reserved.
 //
 
 #include <iostream>
+#include "smart_pointer.h"
+using namespace std;
+
+struct Engine {
+    string imei;
+    int pistons;
+    double capacity;
+    Engine(const int &pistons, const double &capacity, const string &imei) : pistons(pistons), capacity(capacity), imei(imei) {}
+    ~Engine() { cout << "destructor!" << endl; }
+};
+
+void nothing_at_all(Engine *engine) {
+    SmartPointer<Engine> ptr(engine);
+    SmartPointer<Engine> ptr_copy(ptr);
+    cout << "references: " << ptr_copy.use_count() << endl;
+    cout << "from pointer[imei]: " << ptr->imei << endl;
+    cout << "from copied_pointer[imei]: " << (*ptr_copy).imei << endl;
+}
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Engine *engine = new Engine(6, 2401.07, "1234567890");
+    nothing_at_all(engine);
     return 0;
 }
-
---
+++ SmartPointers/smart_pointer.h
@@ -0,0 +1,87 @@
+#ifndef SMART_POINTER_H
+#define SMART_POINTER_H
+//
+//  smart_pointer.h
+//  SmartPointers
+//
+//  Created by Artur Hebda on 13/06/2011.
+//  Copyright 2011 AdTaily. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+
+typedef int Counter;
+
+template <class T>
+class SmartPointer {
+    T *_pointer;
+    Counter *_counter;
+
+    void add_reference();
+    void remove_reference();
+    void remove_object_if_there_are_no_references_left();
+    
+public:
+    Counter use_count() const;
+    T & operator*();
+    T * operator->();
+    T ** operator&();
+    operator bool() const;
+    SmartPointer<T> & operator=(const SmartPointer<T> &source);
+    SmartPointer(T *element) : _pointer(element), _counter(new Counter(0)) { add_reference(); }
+    SmartPointer(const SmartPointer<T> &source) : _pointer(source._pointer), _counter(source._counter) { add_reference(); }
+    ~SmartPointer() { remove_reference(); remove_object_if_there_are_no_references_left(); }
+};
+
+template <class T>
+void SmartPointer<T>::add_reference() {
+    (*_counter)++;
+}
+
+template <class T>
+void SmartPointer<T>::remove_reference() {
+    (*_counter)--;
+}
+
+template <class T>
+void SmartPointer<T>::remove_object_if_there_are_no_references_left() {
+    if (*_counter == 0) {
+        delete _pointer;
+    }
+}
+
+template <class T>
+Counter SmartPointer<T>::use_count() const {
+    return *_counter;
+}
+
+template <class T>
+T & SmartPointer<T>::operator*() {
+    return *_pointer;
+}
+
+template <class T>
+T * SmartPointer<T>::operator->() {
+    return _pointer;
+}
+
+template <class T>
+T ** SmartPointer<T>::operator&() {
+    return &_pointer;
+}
+
+template <class T>
+SmartPointer<T>::operator bool() const {
+    return _pointer != NULL;
+}
+
+template <class T>
+SmartPointer<T> & SmartPointer<T>::operator=(const SmartPointer<T> &source) {
+    _pointer = source._pointer;
+    _counter = source._counter;
+
+    add_reference();
+    return *this;
+}
+
+#endif
