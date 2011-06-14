2c1b62cf4cd3c6f804fb882b64a8c79f6c15c679
Artur Hebda <artur@adtaily.com>
2011-06-14 16:28:40 +0200

    Initial Commit

---
+++ SmartPointers/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SmartPointers
+//
+//  Created by Artur Hebda on 14/06/2011.
+//  Copyright 2011 AdTaily. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
